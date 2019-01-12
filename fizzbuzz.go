585d0f253216ff4ee764abd8f625ae30701b0218
@paripol <paripol@humanize.co.th>
2019-01-12 14:34:54 +0700

    commit 'cases 2'

---
+++ fizzbuzz.go
@@ -0,0 +1,10 @@
+package fizzbuzz
+
+func Say(n int) string {
+	if n == 1 {
+		return "1"
+	} else if n == 2 {
+		return "2"
+	}
+	return "0"
+}
---
+++ fizzbuzz_test.go
@@ -0,0 +1,22 @@
+package fizzbuzz_test
+import (
+	"testing"
+	"github.com/niiiz/fizzbuzz"
+)
+func TestFizzBuzz(t *testing.T){
+	t.Run("1 should say 1", func(t *testing.T){
+		
+		want := "1"
+		get := fizzbuzz.Say(1)
+		if want != get {
+			t.Errorf("it should say %s but get %s", want, get)
+		}
+	})
+	t.Run("2 should say 2", func(t *testing.T){
+		want := "2"
+		get := fizzbuzz.Say(2)
+		if want != get {
+			t.Errorf("it should say %s but get %s", want, get)
+		}
+	})
+}
